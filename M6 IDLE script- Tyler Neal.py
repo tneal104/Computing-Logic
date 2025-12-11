5f07f82aa46f87a3cb37b3e859e2f6bffc5e1e3e
tneal104 <tneal104@ivytech.edu>
2025-12-10 23:01:12 -0500

    Add files via upload

---
+++ M6 IDLE script- Tyler Neal.py
@@ -0,0 +1,10 @@
+"""This code will
+set vairable c, d, and s
+print a message"""
+#c is the word "cats"
+c=" cats "
+#d is the word "dogs"
+d=" dogs "
+#s is the sentence to print
+s="It is raining" + c + "and" + d + "."
+print(s)
